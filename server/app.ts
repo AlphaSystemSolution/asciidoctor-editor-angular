e3cb08c53c229e89e8c799bb674fa42644aeeb00
sfali23 <f.syed.ali@gmail.com>
2016-08-23 09:02:06 -0400

    updatec with "data" routes

--- server/app.ts
+++ server/app.ts
@@ -4,6 +4,10 @@ import { join } from "path";
 import * as favicon from "serve-favicon";
 import { json, urlencoded } from "body-parser";
 
+import * as dataRoute from './mongo';
+
+let router: express.Router = express.Router();
+
 const app: express.Application = express();
 app.disable("x-powered-by");
 
@@ -15,6 +19,13 @@ app.use(urlencoded({ extended: true }));
 
 app.use('/client', express.static(join(__dirname, '../client')));
 
+// data route binds
+var route: dataRoute.Route = new dataRoute.Route();
+router.post("/authenticate", route.authenticate.bind(route.authenticate));
+router.post("/folders", route.findChildFolders.bind(route.findChildFolders));
+router.post("/createFolder", route.createFolder.bind(route.createFolder));
+app.use("/data", router);
+
 // error handlers
 // development error handler
 // will print stacktrace
@@ -22,7 +33,7 @@ if (app.get("env") === "development") {
 
     app.use(express.static(join(__dirname, '../node_modules')));
 
-    app.use(function(err, req: express.Request, res: express.Response, next: express.NextFunction) {
+    app.use(function (err, req: express.Request, res: express.Response, next: express.NextFunction) {
         res.status(err.status || 500);
         res.json({
             error: err,
@@ -32,14 +43,14 @@ if (app.get("env") === "development") {
 }
 
 // catch 404 and forward to error handler
-app.use(function(req: express.Request, res: express.Response, next) {
+app.use(function (req: express.Request, res: express.Response, next) {
     let err = new Error("Not Found");
     next(err);
 });
 
 // production error handler
 // no stacktrace leaked to user
-app.use(function(err: any, req: express.Request, res: express.Response, next: express.NextFunction) {
+app.use(function (err: any, req: express.Request, res: express.Response, next: express.NextFunction) {
     res.status(err.status || 500);
     res.json({
         error: {},
